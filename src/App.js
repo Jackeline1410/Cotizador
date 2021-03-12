015ef1a755e4af5190e178e58679a314f1238b2f
Jackeline <ing.jacke@gmail.com>
2021-03-12 16:54:54 -0500

    Creando el formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+    background-color: #FFFFFF;
+    padding: 3rem;
+`
 
 function App() {
   return (
-    <Header 
+    <Contenedor>
+    <Header
     titulo='Cotizador de Seguros'
     />
+    <ContenedorFormulario>
+      <Formulario />
+    </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
