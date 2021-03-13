70b7209b981ef177df77aff11b170d52cfb5dda9
Jackeline <ing.jacke@gmail.com>
2021-03-13 17:18:15 -0500

    Calculando el costo basado en años

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import styled from '@emotion/styled';
+import { obtenerDiferenciaYear } from '../helper';
 
 const Campo = styled.div`
     display: flex;
@@ -80,9 +81,17 @@ const cotizarSeguro = e => {
     }
     guardarError(false);
 
+    // Una base de 2000
+    let resultado = 2000;
+
     // obtener la diferencia de años
+    const diferencia = obtenerDiferenciaYear(year);
+    console.log(diferencia);
 
     // por cada año hay que restar el 3% del valor
+    resultado -= ((diferencia * 3) * resultado )/100;
+    console.log(resultado)
+
 
     // El Americano sera un 15%
 
